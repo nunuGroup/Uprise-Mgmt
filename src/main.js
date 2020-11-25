e103a892dfb4bc38712fa4657cbe8c5801641a7c
Trent Brew <tbrew1023@gmail.com>
2020-11-25 14:48:33 -0600

    migration

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueFullPage from 'vue-fullpage.js'
+
+Vue.config.productionTip = false
+
+Vue.use(VueFullPage);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
